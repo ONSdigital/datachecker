8ff8433e5b9f72c0b3046a5e2af72af2adb45af8
jday7879 <57715292+Jday7879@users.noreply.github.com>
2026-02-25 15:56:00 +0000

    new test to check pyspark log

--- tests/test_pyspark_validator.py
+++ tests/test_pyspark_validator.py
@@ -107,3 +107,59 @@ class TestPysparkValidator:
 
         # Clean up
         os.remove("temp.html")
+
+    def test_pyspark_validate_boilerplate_checks(self):
+        from datachecker.data_checkers.pyspark_validator import PySparkValidator
+
+        df = pd.DataFrame(
+            {
+                "id": [1, 2.2, 332, 2],
+                "name": ["Alice", "Bob", "Charlie", "Bob"],
+                "score": [90.5, 82.24, 95.25, 182.0],
+                "passed": [True, False, True, True],
+            }
+        )
+        spark_df = self.spark.createDataFrame(df)
+
+        schema = {
+            "check_duplicates": False,
+            "check_completeness": False,
+            "columns": {
+                "id": {
+                    "type": "int",
+                    "allow_na": False,
+                    "max_val": 2,
+                    "min_val": 0,
+                    "optional": False,
+                },
+                "name": {
+                    "type": "str",
+                    "allow_na": False,
+                    "optional": False,
+                    "min_length": 4,
+                    "max_length": 10,
+                },
+                "score": {
+                    "type": "int",
+                    "allow_na": False,
+                    "min_val": 0,
+                    "max_val": 100,
+                    "max_decimal": 5,
+                    "min_decimal": 2,
+                    "optional": False,
+                },
+                "passed": {"type": "bool", "allow_na": False, "optional": False},
+            },
+        }
+        new_validator = PySparkValidator(
+            schema=schema, data=spark_df, file="temp.html", format="html", hard_check=False
+        )
+        new_validator.validate()
+        new_validator.export()
+
+        assert isinstance(new_validator, PySparkValidator)
+        assert len(new_validator.log) > 0
+        assert os.path.exists("temp.html")
+
+        # Clean up
+        os.remove("temp.html")
