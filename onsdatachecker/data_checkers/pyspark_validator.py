f572509eb9e1dcffdfcbbc60e8479b9926d6a508
jday7879 <57715292+Jday7879@users.noreply.github.com>
2026-05-29 14:28:03 +0100

    Update docstrings for polars, pandas and pyspark classes

--- onsdatachecker/data_checkers/pyspark_validator.py
+++ onsdatachecker/data_checkers/pyspark_validator.py
@@ -5,6 +5,21 @@ from onsdatachecker.data_checkers.general_validator import Validator
 
 
 class PySparkValidator(Validator):
+    """
+    `PySparkValidator` is a subclass of Validator specifically for validating data loaded
+    using pyspark.
+    It inherits all methods and attributes from the Validator class.
+
+    Methods
+    -------
+    __init__(schema, data, file, format, hard_check, custom_checks)
+        Initializes the DataValidator class with the provided parameters.
+    _check_duplicates()
+        Checks for duplicate rows in the dataframe based on the specified columns in the schema.
+    _check_completeness()
+        Checks for missing combinations of values in specified columns to ensure data completeness.
+    """
+
     def __init__(
         self,
         schema: dict,
@@ -14,6 +29,25 @@ class PySparkValidator(Validator):
         hard_check: bool = True,
         custom_checks: dict = None,
     ):
+        """
+        initialise class
+
+        Parameters
+        ----------
+        schema : dict or str
+            The schema or path to schema file to validate against.
+        data : pd.DataFrame
+            The data to be validated.
+        file : str
+            The file path for exporting validation logs.
+        format : str
+            The format to use when exporting logs.
+        hard_check : bool, optional
+            Determines if strict validation is enforced will stop pipeline if error or warning
+            detected if set to true, by default True
+        custom_checks : dict, optional
+            any custom checks to run, by default None
+        """
         warnings.warn(
             "PySparkValidator is in early stages of development. "
             "Please open issues on GitHub if you encounter any strange or broken behaviour",
@@ -70,6 +104,10 @@ class PySparkValidator(Validator):
             )
 
     def _check_duplicates(self):
+        """
+        checks for duplicate rows within the dataframe. Updates log entry to highlight
+        status of check
+        """
         from pyspark.sql import functions as F
 
         # Check for duplicate rows in the dataframe
@@ -88,6 +126,11 @@ class PySparkValidator(Validator):
             )
 
     def _check_completeness(self):
+        """
+        checks dataframe for completeness i.e. checks combinations of values in each column
+        to see if each possible combination is present in the data, does not return information
+        on missing combinations.
+        """
         from pyspark.sql import functions as F
 
         if self.schema.get("check_completeness", False):
